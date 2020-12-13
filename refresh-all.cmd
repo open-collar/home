a588260c75046b97e144716122fe9b71319f93ed
Jonathan Evans <jevans@open-collar.org.uk>
2020-12-13 14:16:11 +0000

    Add script to simplify bulk refreshes from git.

---
+++ refresh-all.cmd
@@ -0,0 +1,69 @@
+cd ..
+
+cd findlost
+git.exe pull --progress -v --no-rebase "origin"
+cd ..
+
+cd home
+git.exe pull --progress -v --no-rebase "origin"
+cd ..
+
+cd onyct
+git.exe pull --progress -v --no-rebase "origin"
+cd ..
+
+cd OpenCollar.Azure.ReliableQueue
+git.exe pull --progress -v --no-rebase "origin"
+cd ..
+
+cd OpenCollar.Azure.Storage
+git.exe pull --progress -v --no-rebase "origin"
+cd ..
+
+cd OpenCollar.Extensions
+git.exe pull --progress -v --no-rebase "origin"
+cd ..
+
+cd OpenCollar.Extensions.ApplicationInsights
+git.exe pull --progress -v --no-rebase "origin"
+cd ..
+
+cd OpenCollar.Extensions.Collections
+git.exe pull --progress -v --no-rebase "origin"
+cd ..
+
+cd OpenCollar.Extensions.Configuration
+git.exe pull --progress -v --no-rebase "origin"
+cd ..
+
+cd OpenCollar.Extensions.Environment
+git.exe pull --progress -v --no-rebase "origin"
+cd ..
+
+cd OpenCollar.Extensions.IO
+git.exe pull --progress -v --no-rebase "origin"
+cd ..
+
+cd OpenCollar.Extensions.Logging
+git.exe pull --progress -v --no-rebase "origin"
+cd ..
+
+cd OpenCollar.Extensions.Security
+git.exe pull --progress -v --no-rebase "origin"
+cd ..
+
+cd OpenCollar.Extensions.SqlClient
+git.exe pull --progress -v --no-rebase "origin"
+cd ..
+
+cd OpenCollar.Extensions.Threading
+git.exe pull --progress -v --no-rebase "origin"
+cd ..
+
+cd OpenCollar.Extensions.Validation
+git.exe pull --progress -v --no-rebase "origin"
+cd ..
+
+cd PlanRep
+git.exe pull --progress -v --no-rebase "origin"
+cd ..
